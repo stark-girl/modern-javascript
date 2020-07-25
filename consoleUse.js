2d799efdb44d5894d809fd35ca573a48476fc7b4
Priyanshi <singh.priyan24@gmail.com>
2020-07-25 23:44:19 +0530

    Added files for initial commit

---
+++ consoleUse.js
@@ -0,0 +1,23 @@
+console.log('Hello World');
+console.log(true);
+console.log(123);
+var greeting = 'hey';
+console.log(greeting);
+console.log([1,2,3]);
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+
+
+
+console.error("This is error");
+console.clear();
+console.warn('This is warning');
+
+console.time('Hello')
+    console.log('HelloWorld')
+    console.log('HelloWorld')
+    console.log('HelloWorld')
+    console.log('HelloWorld')
+    console.log('HelloWorld')
+    console.log('HelloWorld')
+console.timeEnd("Hello")
