a7f30b22e1e0f8462dbed2b7fb15da82b446fd05
Priyanshi <singh.priyan24@gmail.com>
2020-07-26 00:16:14 +0530

    Added more files

---
+++ dataType.js
@@ -0,0 +1,25 @@
+//PRIMITIVE Type
+
+const name ='John doe';
+
+console.log(typeof name);
+
+const hasKids = false;
+
+console.log( typeof hasKids);
+
+const car = null;
+
+console.log(typeof car);//Bug
+
+let test;
+const symw =  Symbol();
+
+
+const hobbies=['hrllo','huhu'];
+const address={
+    city:'Boston',
+    state:'MA'
+}
+
+console.log(typeof address)
---
+++ varLetConst.js
@@ -0,0 +1,43 @@
+var name ='John doe';
+console.log(name);
+name='Steve Smith';
+console.log(name);
+
+// //Init var
+var greeting;
+console.log(greeting);
+greeting= 'Hello';
+console.log(greeting);
+
+// //can contain letters, numbers, _, $
+// //cannot start with number
+
+// //Multi words variable
+
+// var firstName='John';
+
+//LET
+
+let  name ='John doe';
+console.log(name);
+name='Steve Smith';
+console.log(name);
+
+//const
+
+const greetig = "Hello";
+console.log(greetig);
+//const hello;//will give error;
+//greeting = "melloe"//will give error//
+
+const person ={
+    name:'John',
+    age:30
+}
+
+person.name='Sara';
+
+console.log(person);
+
+//we change inside things and field like in array or in object
+
