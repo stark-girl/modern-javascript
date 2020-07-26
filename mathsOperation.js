2d6fdc33de4eef6950d22dc5617255a01827e530
Priyanshi <singh.priyan24@gmail.com>
2020-07-26 13:09:01 +0530

    Added Math operations

---
+++ mathsOperation.js
@@ -0,0 +1,21 @@
+//Maths operation
+num1= 10;
+num2= 5;
+
+num= num1 % num2;
+num= num1 -num2;
+num= num1 * num2;
+num= num1 +num2;
+num= num1 / num2;
+
+num = Math.floor(56.89);
+num = Math.ceil(453.36);
+num = Math.round(456.66);
+num = Math.sqrt(16);
+num = Math.max(2,3,4,5,6)//Cannot Take array
+num= Math.min(2,3);
+num = Math.floor(Math.random() * 20);
+
+
+console.log(num)
+
