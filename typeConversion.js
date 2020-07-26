6f66fa323c0bb22ebfff0b8f61ee62490dd5d205
Priyanshi <singh.priyan24@gmail.com>
2020-07-26 12:45:49 +0530

    Added typeConversion js

---
+++ typeConversion.js
@@ -0,0 +1,41 @@
+// Number to String
+ 
+val =5;
+
+val = String(val)
+val = String(4+4);
+
+//Bool to String
+
+val = String(true);
+
+//Date to String
+
+val = String(new Date());
+
+//Array to String
+
+val = String([1,2,3]);
+
+//toString()
+val=(5).toString();
+val= (true).toString();
+val= Number(5);
+val = Number(true);
+
+val = parseFloat('100.30');
+//val = parseInt('100.30')
+
+//String to Number
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+console.log(val.toFixed(2));
+
+val1= String(6);
+val2 = '8';
+
+sum = val1 + val2;
+
+console.log(sum);
