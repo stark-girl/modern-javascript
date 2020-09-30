c545d30a12fea93a90bf0e1df585c16b7cc3517d
Anuj Singh <singhanuj620@gmail.com>
2020-10-01 00:46:30 +0530

    Create arrowFunction.js

---
+++ arrowFunction.js
@@ -0,0 +1,21 @@
+// Default Function :
+
+var hello;
+hello = function() {
+  return "Hello World!";
+}
+
+hello();
+
+// Output = Hello World
+
+
+// Arrow Function :
+
+hello = () => {
+  return "Hello World!";
+}
+
+hello();
+
+// Output = Hello World
